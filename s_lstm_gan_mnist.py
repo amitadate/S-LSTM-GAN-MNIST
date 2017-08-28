bebace51fa117895acba4ace729f91c91bd461e0
Amit Adate <email2amitadate@gmail.com>
2017-08-29 02:43:35 +0530

    Update and rename lstm_gan_mnist.py to s_lstm_gan_mnist.py
    Modules imoorted

---
+++ s_lstm_gan_mnist.py
@@ -0,0 +1,15 @@
+import tensorflow as tf 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import Mnist
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("D:/Reasearch/GAN/Code", one_hot=True)
+
+class s_lstm_gan_mnist_model(object):
+  
+  
+  
+
